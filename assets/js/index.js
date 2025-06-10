6353ddf113238456be4e91df8af75cc395217d68
BelickijDmitry <belickij@jam-it.ru>
2025-06-10 17:55:47 +0300

    до футера

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,32 @@ document.addEventListener('DOMContentLoaded', function () {
       burger.classList.toggle('active');
       mobileMenu.classList.toggle('active');
     });
+
+
+    const select = document.getElementById('roleSelect');
+    const trigger = document.getElementById('selectedRole');
+    const options = document.querySelectorAll('.custom-option');
+    const roleInput = document.getElementById('roleInput');
+  
+    select.addEventListener('click', () => {
+      select.classList.toggle('open');
+    });
+  
+    options.forEach(option => {
+      option.addEventListener('click', () => {
+        trigger.textContent = option.textContent;
+        roleInput.value = option.dataset.value;
+        select.classList.remove('open');
+      });
+    });
+  
+    // Закрытие при клике вне
+    document.addEventListener('click', function(event) {
+      if (!select.contains(event.target)) {
+        select.classList.remove('open');
+      }
+    });
+
   });
+
+ 
